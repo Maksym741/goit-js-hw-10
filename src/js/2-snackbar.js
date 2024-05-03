3a06ebde36c02076c1f3a313fd20a762010c72db
Maksym741 <pastushinamaksim@gmail.com>
2024-05-03 22:42:15 +0200

    finish1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,3 @@
-// Функція для створення промісу з вказаною затримкою та станом
 function createPromise(delay, state) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -11,27 +10,18 @@ function createPromise(delay, state) {
   });
 }
 
-// Обробник події сабміту форми
 document.querySelector('.form').addEventListener('submit', function (event) {
-  event.preventDefault(); // Зупиняємо стандартну поведінку форми
-
-  // Отримуємо значення з поля вводу для затримки
+  event.preventDefault();
   const delay = parseInt(this.elements['delay'].value);
-
-  // Отримуємо значення вибраного стану з радіокнопок
   const state = this.elements['state'].value;
-
-  // Створюємо проміс з вказаною затримкою та станом
   createPromise(delay, state)
     .then(delay => {
-      // Якщо проміс виконується вдало
       iziToast.success({
         title: 'Success',
         message: `✅ Fulfilled promise in ${delay}ms`,
       });
     })
     .catch(delay => {
-      // Якщо проміс відхилено
       iziToast.error({
         title: 'Error',
         message: `❌ Rejected promise in ${delay}ms`,
